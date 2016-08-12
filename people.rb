16f56a581a1c5f7bb3bf67e1c71a3b5ea58f077a
mitchell demers <mitchelldemers66@gmail.com>
2016-08-12 10:31:52 -0400

    parent person class was made with the name initialized

--- people.rb
+++ people.rb
@@ -1,11 +1,18 @@
-class Student
+class Person
+	def initialize(name)
+		@name = name
+	end
+end
+
+
+class Student < Person
 	def learn
 		puts "I get it!"
 	end
 end
 
 
-class Instructor
+class Instructor < Person
 	def teach
 		puts "everything in ruby is an Object"
 	end
